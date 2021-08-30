dcbe8abd5abb595787d2a6da911f202f6dcdf95e
Tintinwu <b07901031@ntu.edu.tw>
2021-08-30 17:42:08 +0800

    print dataset size

--- Panoptic-PolarNet/train.py
+++ Panoptic-PolarNet/train.py
@@ -75,12 +75,16 @@ def main(args):
     #prepare dataset
     train_pt_dataset = SemKITTI(data_path + '/sequences/', imageset = 'train', return_ref = True, instance_pkl_path=args['dataset']['instance_pkl_path'])
     val_pt_dataset = SemKITTI(data_path + '/sequences/', imageset = 'val', return_ref = True, instance_pkl_path=args['dataset']['instance_pkl_path'])
+    print("train_pt size = "+str(len(train_pt_dataset)))
+    print("val_pt size = "+str(len(val_pt_dataset)))
     if args['model']['polar']:
         train_dataset=spherical_dataset(train_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0, use_aug = True)
         val_dataset=spherical_dataset(val_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0)
     else:
         train_dataset=voxel_dataset(train_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0,use_aug = True)
         val_dataset=voxel_dataset(val_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0)
+    print("train size = "+str(len(train_dataset)))
+    print("val size = "+str(len(val_dataset)))
     train_dataset_loader = torch.utils.data.DataLoader(dataset = train_dataset,
                                                     batch_size = train_batch_size,
                                                     collate_fn = collate_fn_BEV,
