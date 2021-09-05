d3c8aa0d066086bb930551c89bbb64f2baae7556
Tim <A88551212@gmail.com>
2021-09-05 14:08:39 +0800

    preprocess try

--- Panoptic-PolarNet/preprocess.py
+++ Panoptic-PolarNet/preprocess.py
@@ -51,7 +51,7 @@ def main(args):
         print("train_gt_offset : ", train_gt_offset.shape)
         print(train_gt_offset)
 
-        print("train_grid : ", len(train_grid[1]), len(train_grid[2]))
+        print("train_grid : ", len(train_grid[1]))
         print(train_grid)
 
         print("train_pt_fea : ", len(train_pt_fea[1]), len(train_pt_fea[2]))
