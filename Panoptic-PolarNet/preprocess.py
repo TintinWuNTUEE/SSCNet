f36b107e1fdb28aed11944b7cb4e320f85168fdf
Tim <A88551212@gmail.com>
2021-11-10 16:20:43 +0800

    preprocess change resolution

--- Panoptic-PolarNet/preprocess.py
+++ Panoptic-PolarNet/preprocess.py
@@ -37,8 +37,8 @@ def main(args):
         train_dataset=spherical_dataset(train_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0, use_aug = True)
         val_dataset=spherical_dataset(val_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0)
     else:
-        train_dataset=voxel_dataset(train_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0,use_aug = True)
-        val_dataset=voxel_dataset(val_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0,max_volume_space = [51.2,25.6,6.4], min_volume_space = [0,-25.6,0])
+        train_dataset=voxel_dataset(train_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0,use_aug = True,max_volume_space = [51.2,25.6,4.4], min_volume_space = [0,-25.6,-2])
+        val_dataset=voxel_dataset(val_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0,max_volume_space = [51.2,25.6,4.4], min_volume_space = [0,-25.6,-2])
     print("train size = "+str(len(train_dataset)))
     print("val size = "+str(len(val_dataset)))
     train_dataset_loader = torch.utils.data.DataLoader(dataset = train_dataset,
@@ -58,8 +58,6 @@ def main(args):
         val_gt_center_tensor = val_gt_center.to(device)
         val_gt_offset_tensor = val_gt_offset.to(device)
         
-        for_mask1 = torch.zeros(1,grid_size[0],grid_size[1],grid_size[2],dtype=torch.bool).to(device)
-        for_mask1[(val_label_tensor>=0 )& (val_label_tensor<8)] = True  
         for i in range(len(filenames)):
             label_to_be_save= (val_label_tensor[i].cpu().numpy(),val_gt_center_tensor[i].cpu().numpy(), val_gt_offset_tensor[i].cpu().numpy())
             folder_path,filename = splitPath(filenames[i])
@@ -70,23 +68,23 @@ def main(args):
                 os.makedirs(folder_path)
             save_path = os.path.join(folder_path,filename)
             torch.save(label_to_be_save,save_path)
-    # for _,(_,train_label_tensor,train_gt_center,train_gt_offset,_,_,_,_, filenames)  in enumerate(train_dataset_loader):
-    #     train_label_tensor = SemKITTI2train(train_label_tensor)
-    #     train_label_tensor = train_label_tensor.to(device)
-    #     train_gt_center_tensor = train_gt_center.to(device)
-    #     train_gt_offset_tensor = train_gt_offset.to(device)
-    #     for i in range(len(filenames)):
-    #         label_to_be_save=(train_label_tensor[i].cpu().numpy(),train_gt_center_tensor[i].cpu().numpy(),train_gt_offset_tensor[i].cpu().numpy())
-    #         folder_path,filename = splitPath(filenames[i])
-    #         folder_path=folder_path.replace('velodyne','preprocess')
-    #         filename+='.pt'
-    #         if not os.path.exists(folder_path):
-    #             print(folder_path)
-    #             os.makedirs(folder_path)
-    #         save_path = os.path.join(folder_path,filename)
-    #         torch.save(label_to_be_save,save_path)
-    #         # use labels = np.load("file name", allow_pickle=True) to get data back, the labels is (3,), 
-    #         # and label[0]'s size = (256,256,32)->sem label, label[1]'s size = (1,256,256)->center label, label[2]'s size = (2,256,256)->offset label
+    for _,(_,train_label_tensor,train_gt_center,train_gt_offset,_,_,_,_, filenames)  in enumerate(train_dataset_loader):
+        train_label_tensor = SemKITTI2train(train_label_tensor)
+        train_label_tensor = train_label_tensor.to(device)
+        train_gt_center_tensor = train_gt_center.to(device)
+        train_gt_offset_tensor = train_gt_offset.to(device)
+        for i in range(len(filenames)):
+            label_to_be_save=(train_label_tensor[i].cpu().numpy(),train_gt_center_tensor[i].cpu().numpy(),train_gt_offset_tensor[i].cpu().numpy())
+            folder_path,filename = splitPath(filenames[i])
+            folder_path=folder_path.replace('velodyne','preprocess')
+            filename+='.pt'
+            if not os.path.exists(folder_path):
+                print(folder_path)
+                os.makedirs(folder_path)
+            save_path = os.path.join(folder_path,filename)
+            torch.save(label_to_be_save,save_path)
+            # use labels = np.load("file name", allow_pickle=True) to get data back, the labels is (3,), 
+            # and label[0]'s size = (256,256,32)->sem label, label[1]'s size = (1,256,256)->center label, label[2]'s size = (2,256,256)->offset label
     
     return
 
