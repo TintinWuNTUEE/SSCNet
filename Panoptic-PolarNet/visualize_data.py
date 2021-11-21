3bd005f3bae8209f8f9ceae26dad5ca871031fd7
Tim <A88551212@gmail.com>
2021-11-21 11:27:11 +0800

    preprocess.py is ok now
    preprocess data only contain center and offset

--- Panoptic-PolarNet/visualize_data.py
+++ Panoptic-PolarNet/visualize_data.py
@@ -51,18 +51,19 @@ def create_mask(voxel_label,label_tensor,offset,thing_list):
 def main():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-p','--file_path',default='../semanticKITTI/dataset/sequences')
-    parser.add_argument('-s','--sequence')
-    parser.add_argument('-n','--num',type=int)
-    parser.add_argument('-t','--type',nargs='+',help='preprocess voxel_label')
+    parser.add_argument('-s','--sequence',default='04')
+    parser.add_argument('-n','--num',type=int,default=0)
+    parser.add_argument('-t','--type',nargs='+',help='preprocess voxel_label',default=['preprocess','voxel_label'])
     
     args = parser.parse_args()
     
-    preprocess_file = sorted(glob(os.path.join(args.file_path, args.sequence, 'preprocess_new', "*.pt")))[::5]
+    preprocess_file = sorted(glob(os.path.join(args.file_path, args.sequence, 'preprocess', "*.pt")))
     label_file = sorted(glob(os.path.join(args.file_path, args.sequence, "voxels", "*.label")))
     point_file = sorted(glob(os.path.join(args.file_path, args.sequence, "velodyne","*.bin")))[::5]
     point_label_file = sorted(glob(os.path.join(args.file_path, args.sequence, "labels","*.label")))[::5]
 
     print(args)
+    print(preprocess_file)
     
     dataset_config = yaml.safe_load(open(os.path.join('./semantic-kitti.yaml'), 'r'))
     thing_list = [i for i in dataset_config['thing_class'].keys() if dataset_config['thing_class'][i]==True]
@@ -70,30 +71,44 @@ def main():
     if 'preprocess' in args.type:
         print('into preprocess')
         preprocess_data = list(torch.load(preprocess_file[args.num]))
-        partial_label = preprocess_data[0]
-        center = preprocess_data[1]
-        offset = preprocess_data[2]
-        data = fix_label_tensor(partial_label)
-        mask1 = np.zeros_like(partial_label,dtype=bool)
-        for label in thing_list:
-            mask1[data == label] = True
-        partial_label[~mask1] = 0
-        partial_voxel_bev = ((partial_label>0).sum(axis=2))>0
-        
-        plot0 = plt.figure('partial_voxel_bev')
-        plt.imshow(partial_voxel_bev,cmap=plt.cm.gray,origin='lower')
-        partial_voxel_bev_nonzero = np.nonzero(partial_voxel_bev)
-        # print(partial_voxel_bev_nonzero)
-        for row, col in zip(partial_voxel_bev_nonzero[0],partial_voxel_bev_nonzero[1]):
-            for i in range(32):
-                if partial_label[row, col, i] != 0:
-                    plt.text(col, row, str(data[row, col, i]), color='green',fontsize=12)
-                    break
-        plot1 = plt.figure('center heat map')
-        plt.imshow(center.squeeze(), cmap='hot', origin='lower')
-        
-        plot2, ax = plt.subplots()
-        a = ax.quiver(offset[1,:,:],offset[0,:,:],angles='xy',scale_units='xy',scale=1)
+        if len(preprocess_data) == 3:
+            partial_label = preprocess_data[0]
+            center = preprocess_data[1]
+            offset = preprocess_data[2]
+            inst_data = (partial_label&0xffff0000)>>16
+            data = fix_label_tensor(partial_label)
+            mask1 = np.zeros_like(partial_label,dtype=bool)
+            for label in thing_list:
+                mask1[data == label] = True
+            partial_label[~mask1] = 0
+            partial_voxel_bev = ((partial_label>0).sum(axis=2))>0
+            
+            plot0 = plt.figure('partial_voxel_bev')
+            plt.imshow(partial_voxel_bev,cmap=plt.cm.gray,origin='lower')
+            partial_voxel_bev_nonzero = np.nonzero(partial_voxel_bev)
+            # print(partial_voxel_bev_nonzero)
+            for row, col in zip(partial_voxel_bev_nonzero[0],partial_voxel_bev_nonzero[1]):
+                for i in range(32):
+                    if partial_label[row, col, i] != 0:
+                        # plt.text(col, row, str(data[row, col, i]), color='green',fontsize=12)
+                        plt.text(col, row, str(inst_data[row, col, i]), color='red',fontsize=12)
+                        break
+            plot1 = plt.figure('center heat map')
+            plt.imshow(center.squeeze(), cmap='hot', origin='lower')
+            
+            plot2, ax = plt.subplots()
+            a = ax.quiver(offset[1,:,:],offset[0,:,:],angles='xy',scale_units='xy',scale=1)
+        elif len(preprocess_data) == 2:
+            center = preprocess_data[0]
+            offset = preprocess_data[1]
+            plot1 = plt.figure('center heat map')
+            plt.imshow(center.squeeze(), cmap='hot', origin='lower')
+            
+            plot2, ax = plt.subplots()
+            a = ax.quiver(offset[1,:,:],offset[0,:,:],angles='xy',scale_units='xy',scale=1)
+        else:
+            raise ValueError("preprocess data should be length 2 or length 3")
+
         
     if 'voxel_label' in args.type:
         print('into voxel_label')
@@ -110,11 +125,11 @@ def main():
         plot3 = plt.figure('complete_voxel_bev')
         plt.imshow(complete_voxel_bev,cmap=plt.cm.gray,origin='lower')
         complete_voxel_bev_nonzero = np.nonzero(complete_voxel_bev)
-        for row, col in zip(complete_voxel_bev_nonzero[0],complete_voxel_bev_nonzero[1]):
-            for i in range(32):
-                if voxel_label[row,col,i] != 0:
-                    plt.text(col,row,str(voxel_label[row,col,i]),color='green',fontsize=12)
-                    break
+        # for row, col in zip(complete_voxel_bev_nonzero[0],complete_voxel_bev_nonzero[1]):
+        #     for i in range(32):
+        #         if voxel_label[row,col,i] != 0:
+        #             plt.text(col,row,str(voxel_label[row,col,i]),color='green',fontsize=12)
+        #             break
     print('done')
     plt.show()
     
--- map_labels/preprocess.py
+++ map_labels/preprocess.py
@@ -10,6 +10,14 @@ from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 device=('cuda')
 dataset_config = yaml.safe_load(open(os.path.join('./semantic-kitti.yaml'), 'r'))
+############################## grid size setting ##############################
+max_bound = np.asarray([51.2,25.6,4.4])
+min_bound = np.asarray([0,-25.6,-2])
+crop_range = max_bound-min_bound
+cur_grid_size = np.asarray([256,256,32])
+intervals = crop_range/(cur_grid_size-1)
+############################## grid size setting ##############################
+
 def unpack(compressed):
     ''' given a bit encoded voxel grid, make a normal voxel grid out of it.  '''
     uncompressed = np.zeros(compressed.shape[0] * 8, dtype=np.uint8)
@@ -81,19 +89,22 @@ def main(args):
     val_batch_size = args['model']['val_batch_size']
 
     #prepare dataset
-    train_pt_dataset = SemKITTI(data_path + '/sequences/', imageset = 'train', return_ref = True, instance_pkl_path=args['dataset']['instance_pkl_path'])
+    
+    ###################### I change the semantic-kitti.yaml to make val dataloader contain sequence 0-10
+    
+    # train_pt_dataset = SemKITTI(data_path + '/sequences/', imageset = 'train', return_ref = True, instance_pkl_path=args['dataset']['instance_pkl_path'])
     val_pt_dataset = SemKITTI(data_path + '/sequences/', imageset = 'val', return_ref = True, instance_pkl_path=args['dataset']['instance_pkl_path'])
-    print("train_pt size = "+str(len(train_pt_dataset)))
+    # print("train_pt size = "+str(len(train_pt_dataset)))
     print("val_pt size = "+str(len(val_pt_dataset)))
-    train_dataset=voxel_dataset(train_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0,use_aug = True,max_volume_space = [51.2,25.6,4.4], min_volume_space = [0,-25.6,-2])
+    # train_dataset=voxel_dataset(train_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0,use_aug = True,max_volume_space = [51.2,25.6,4.4], min_volume_space = [0,-25.6,-2])
     val_dataset=voxel_dataset(val_pt_dataset, args['dataset'], grid_size = grid_size, ignore_label = 0,max_volume_space = [51.2,25.6,4.4], min_volume_space = [0,-25.6,-2])
-    print("train size = "+str(len(train_dataset)))
+    # print("train size = "+str(len(train_dataset)))
     print("val size = "+str(len(val_dataset)))
-    train_dataset_loader = torch.utils.data.DataLoader(dataset = train_dataset,
-                                                    batch_size = batch_size,
-                                                    collate_fn = collate_fn_BEV,
-                                                    shuffle = False,
-                                                    num_workers = 4)
+    # train_dataset_loader = torch.utils.data.DataLoader(dataset = train_dataset,
+    #                                                 batch_size = batch_size,
+    #                                                 collate_fn = collate_fn_BEV,
+    #                                                 shuffle = False,
+    #                                                 num_workers = 4)
     val_dataset_loader = torch.utils.data.DataLoader(dataset = val_dataset,
                                                     batch_size = val_batch_size,
                                                     collate_fn = collate_fn_BEV,
@@ -103,47 +114,7 @@ def main(args):
     indice = torch.from_numpy(np.repeat(indice[:,:,np.newaxis,:], 32, axis=2)).to(device)
     remap_lut = get_remap_lut()
     knn = KNeighborsClassifier(n_jobs=4)
-    for _,(_,train_label_tensor,train_gt_center,train_gt_offset,_,_,_,_, filenames)  in enumerate(train_dataset_loader):
-        train_label_tensor = SemKITTI2train(train_label_tensor)
-        train_label_tensor = train_label_tensor.to(device)
-        train_gt_center_tensor = train_gt_center.to(device)
-        train_gt_offset_tensor = train_gt_offset.to(device)
-        for i in range(len(filenames)):
-            voxel_label_path = filenames[i].replace('velodyne','voxels').replace('.bin', '.label')
-            if(os.path.isfile(voxel_label_path)==False):
-                continue
-            voxel_label = np.fromfile(voxel_label_path, dtype=np.uint16).reshape(256,256,32)
-            voxel_label =torch.from_numpy(remap_lut[voxel_label.astype(np.uint16)]).to(device)
-            train_label = train_label_tensor[i]
-            voxel_label,mask1,mask2 = mask(voxel_label,train_label)
-            
-            partial_label = (torch.cat((train_label[:,:,:,np.newaxis],indice),axis=3)[mask2]).reshape(-1,3)
-            complete_label = (torch.cat((voxel_label[:,:,:,np.newaxis],indice),axis=3)[mask1]).reshape(-1,3)
-            knn.fit(partial_label.cpu().numpy()[:,1:], partial_label.cpu().numpy()[:,0])
-            predict = knn.predict(complete_label.cpu().numpy()[:,1:])
-            
-            voxel_label,mask1 = voxel_label.cpu().numpy(),mask1.cpu().numpy()
-            voxel_label[mask1]=predict
-            voxel_label = SemKITTI2train(voxel_label)
-            ########################## plotting################################
-            # bev = (voxel_label>0).sum(axis=2)
-            # t4 = plt.figure(5)
-            # plt.imshow(bev,cmap=plt.cm.gray,origin='lower')
-            # for i in range(0,predict.shape[0],20):
-            #     plt.text( complete_label[i,2],complete_label[i,1], str(predict[i]),color="red",fontsize=8)
-
-            # print("done")
-            # plt.show()
-            ########################## plotting################################
-            label_to_be_save= (voxel_label,train_gt_center_tensor[i].cpu().numpy(), train_gt_offset_tensor[i].cpu().numpy())
-            folder_path,filename = splitPath(filenames[i])
-            folder_path=folder_path.replace('velodyne','preprocess')
-            filename+='.pt'
-            if not os.path.exists(folder_path):
-                print(folder_path)
-                os.makedirs(folder_path)
-            save_path = os.path.join(folder_path,filename)
-            torch.save(label_to_be_save,save_path)
+    PanopticLabelGenerator = PanopticLabelGenerator_VoxelVersion((256,256,32))
     for _,(_,val_vox_label,val_gt_center,val_gt_offset,val_grid,_,_,_,filenames) in enumerate(val_dataset_loader):
         val_vox_label = SemKITTI2train(val_vox_label)
         val_label_tensor=val_vox_label.to(device)
@@ -159,15 +130,18 @@ def main(args):
             
             val_label = val_label_tensor[i]
             voxel_label,mask1,mask2 = mask(voxel_label,val_label)
-            
-            partial_label = (torch.cat((val_label[:,:,:,np.newaxis],indice),axis=3)[mask2]).reshape(-1,3)
-            complete_label = (torch.cat((voxel_label[:,:,:,np.newaxis],indice),axis=3)[mask1]).reshape(-1,3)
-            knn.fit(partial_label.cpu().numpy()[:,1:], partial_label.cpu().numpy()[:,0])
-            predict = knn.predict(complete_label.cpu().numpy()[:,1:])
-            
-            voxel_label,mask1 = voxel_label.cpu().numpy(),mask1.cpu().numpy()
-            voxel_label[mask1]=predict
-            voxel_label = SemKITTI2train(voxel_label)
+            if (~mask1).all() or (~mask2).all():
+                voxel_label,mask1 = voxel_label.cpu().numpy(),mask1.cpu().numpy()
+                pass
+            else:
+                partial_label = (torch.cat((val_label[:,:,:,np.newaxis],indice),axis=3)[mask2]).reshape(-1,3)
+                complete_label = (torch.cat((voxel_label[:,:,:,np.newaxis],indice),axis=3)[mask1]).reshape(-1,3)
+                knn.fit(partial_label.cpu().numpy()[:,1:], partial_label.cpu().numpy()[:,0])
+                predict = knn.predict(complete_label.cpu().numpy()[:,1:])
+                
+                voxel_label,mask1 = voxel_label.cpu().numpy(),mask1.cpu().numpy()
+                voxel_label[mask1]=predict
+            # voxel_label = SemKITTI2train(voxel_label)
             ########################## plotting################################
             # bev = (voxel_label>0).sum(axis=2)
             # t4 = plt.figure(5)
@@ -178,17 +152,70 @@ def main(args):
             # print("done")
             # plt.show()
             ########################## plotting################################
-            label_to_be_save= (voxel_label,val_gt_center_tensor[i].cpu().numpy(), val_gt_offset_tensor[i].cpu().numpy())
+            ########################## update center offset ################################
+            center, offset = PanopticLabelGenerator(voxel_label,min_bound,intervals,mask1)
+            ########################## update center offset ################################
+            ########################## preprocess only have center and offset ################################
+            label_to_be_save= (center,offset)
             folder_path,filename = splitPath(filenames[i])
             folder_path=folder_path.replace('velodyne','preprocess')
             filename+='.pt'
             if not os.path.exists(folder_path):
-                print(folder_path)
                 os.makedirs(folder_path)
             save_path = os.path.join(folder_path,filename)
             torch.save(label_to_be_save,save_path)
     
     return 
+
+class PanopticLabelGenerator_VoxelVersion():
+    def __init__(self,grid_size,sigma=5):
+        self.grid_size = grid_size
+        self.sigma = sigma
+        size = 6 * sigma + 3
+        x = np.arange(0,size,1,float)
+        y = x[:,np.newaxis]
+        x0,y0 = 3 * sigma + 1, 3 * sigma + 1
+        self.g = np.exp(-((x-x0) ** 2 + (y - y0) ** 2) / (2 * sigma **2))
+        self.indice = np.moveaxis(np.array(np.meshgrid(np.arange(0,256,1),np.arange(0,256,1))),(0,1,2),(2,1,0))
+
+        
+    def __call__(self, voxel_label, min_bound, intervals, mask):
+        height, width = self.grid_size[0], self.grid_size[1]
+        center_pts = []
+        center = np.zeros((1, height, width), dtype=np.float32)
+        offset = np.zeros((2, height, width), dtype=np.float32)
+        inst_data = np.zeros(voxel_label.shape, dtype=np.float32)
+        inst_data[mask] = (voxel_label[mask] & 0xffff0000)>>16
+        inst_labels = np.unique(inst_data)
+        if 0 in inst_labels:
+            inst_labels = inst_labels[inst_labels!=0] #delete instance label 0, it isn't foreground instance
+        if inst_labels.size == 0:
+            return center, offset
+        for inst_label in inst_labels:
+            mask_ = np.where(inst_data==inst_label)
+            center_x, center_y = np.mean(mask_[0]), np.mean(mask_[1])
+            # generate center heatmap
+            x, y = int(np.floor(center_x)), int(np.floor(center_y))
+            center_pts.append([x, y])
+            if x < 0 or y < 0 or x >= height or y >= width:
+                continue
+            sigma = self.sigma
+            # upper left
+            ul = int(np.round(x - 3 * sigma - 1)), int(np.round(y - 3 * sigma - 1))
+            # bottom right
+            br = int(np.round(x + 3 * sigma + 2)), int(np.round(y + 3 * sigma + 2))
+            
+            c, d = max(0,-ul[0]), min(br[0], height) - ul[0]
+            a, b = max(0,-ul[1]), min(br[1], width) - ul[1]
+            
+            cc, dd = max(0,ul[0]), min(br[0], height)
+            aa, bb = max(0, ul[1]), min(br[1], width)
+            
+            center[0, cc:dd, aa:bb] = np.maximum(center[0, cc:dd, aa:bb], self.g[c:d,a:b])
+            offset[0,mask_[0],mask_[1]] = (center_x - mask_[0])
+            offset[1,mask_[0],mask_[1]] = (center_y - mask_[1])
+        return center, offset
+
 
         
 if __name__ == '__main__':
