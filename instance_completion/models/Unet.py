016266591e6e6112a3143d696a4666751a61b18d
Tim <A88551212@gmail.com>
2022-01-08 15:08:26 +0800

    segmentationhead model upload

--- instance_completion/models/Unet.py
+++ instance_completion/models/Unet.py
@@ -42,4 +42,42 @@ class Unet(nn.Module):
         out = self.map(out)
         
         return out
-        
+    
+class SegmentationHead(nn.Module):
+    '''
+    3D Segmentation heads to retrieve semantic segmentation at each scale.
+    Formed by Dim expansion, Conv3D, ASPP block, Conv3D.
+    '''
+    def __init__(self, inplanes, planes, nbr_classes, dilations_conv_list):
+        super().__init__()
+
+        # First convolution
+        self.conv0 = nn.Conv3d(inplanes, planes, kernel_size=3, padding=1, stride=1)
+
+        # ASPP Block
+        self.conv_list = dilations_conv_list
+        self.conv1 = nn.ModuleList(
+        [nn.Conv3d(planes, planes, kernel_size=3, padding=dil, dilation=dil, bias=False) for dil in dilations_conv_list])
+        self.bn1 = nn.ModuleList([nn.BatchNorm3d(planes) for dil in dilations_conv_list])
+        self.conv2 = nn.ModuleList(
+        [nn.Conv3d(planes, planes, kernel_size=3, padding=dil, dilation=dil, bias=False) for dil in dilations_conv_list])
+        self.bn2 = nn.ModuleList([nn.BatchNorm3d(planes) for dil in dilations_conv_list])
+        self.relu = nn.ReLU(inplace=True)
+
+        # Convolution for output
+        self.conv_classes = nn.Conv3d(planes, nbr_classes, kernel_size=3, padding=1, stride=1)
+
+    def forward(self, x_in):
+        # x_in = [B,1,80,80,32]
+
+        # Convolution to go from inplanes to planes features...
+        x_in = self.relu(self.conv0(x_in)) # x_in = [B,2,80,80,32], all same below
+
+        y = self.bn2[0](self.conv2[0](self.relu(self.bn1[0](self.conv1[0](x_in)))))
+        for i in range(1, len(self.conv_list)):
+            y += self.bn2[i](self.conv2[i](self.relu(self.bn1[i](self.conv1[i](x_in)))))
+            
+        x_in = self.relu(y + x_in)  # modified
+        x_in = self.conv_classes(x_in)
+
+        return x_in
